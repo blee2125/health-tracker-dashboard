19aa51cce45bbeeaadc986df2fb900938a74f18e
Brandon Lee <blee2125@gmail.com>
2022-09-27 18:54:49 -0600

    add more fields to food input form

--- src/components/Food/FoodForm.js
+++ src/components/Food/FoodForm.js
@@ -11,9 +11,69 @@ export const FoodForm = (props) => {
         <br />
         <input 
           name='name' 
+          placeholder='name'
           type='text'
-          value={props.name}
-          onChange={e => props.updateData('name', e.target.value)}
+          onChange={e => props.updateData('foodName', e.target.value)}
+        />
+        <input 
+          name='totalCalories'
+          placeholder='totalCalories'
+          type='text'
+          onChange={e => props.updateData('totalCalories', e.target.value)}
+        />
+        <input 
+          name='timeOfConsumption'
+          placeholder='timeOfConsumption'
+          type='text'
+          onChange={e => props.updateData('timeOfConsumption', e.target.value)}
+        />
+        <input 
+          name='meal'
+          placeholder='meal'
+          type='text'
+          onChange={e => props.updateData('meal', e.target.value)}
+        />
+        <input 
+          name='placeOfConsumption'
+          placeholder='placeOfConsumption'
+          type='text'
+          onChange={e => props.updateData('placeOfConsumption', e.target.value)}
+        />
+        <input 
+          name='withWhom'
+          placeholder='withWhom'
+          type='text'
+          onChange={e => props.updateData('withWhom', e.target.value)}
+        />
+        <input 
+          name='activity'
+          placeholder='activity'
+          type='text'
+          onChange={e => props.updateData('activity', e.target.value)}
+        />
+        <input 
+          name='mood'
+          placeholder='mood'
+          type='text'
+          onChange={e => props.updateData('mood', e.target.value)}
+        />
+        <input 
+          name='hungerLevel'
+          placeholder='hungerLevel'
+          type='text'
+          onChange={e => props.updateData('hungerLevel', e.target.value)}
+        />
+        <input 
+          name='fullness'
+          placeholder='fullness'
+          type='text'
+          onChange={e => props.updateData('fullness', e.target.value)}
+        />
+        <input 
+          name='amount'
+          placeholder='amount'
+          type='text'
+          onChange={e => props.updateData('amount', e.target.value)}
         />
       </form>
     );
