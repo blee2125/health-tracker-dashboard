709ed6e633fef451ccdb8c6e44ba146e706cb1aa
Brandon Lee <blee2125@gmail.com>
2022-09-30 13:35:12 -0600

    add future links to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const NavBar = () => {
     
@@ -10,8 +12,27 @@ const NavBar = () => {
             <Container>
                 <Navbar.Brand href="#home">Health Tracker Dashboard</Navbar.Brand>
                 <Nav className="me-auto">
-
+                    <NavDropdown title="Exercise" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#doesnotwork">Exercise Home</NavDropdown.Item>
+                        <NavDropdown.Item href="#doesnotwork">Add Exercise</NavDropdown.Item>
+                        <NavDropdown.Item href="#doesnotwork">another link</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Nutrition" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#doesnotwork">Nutrition Home</NavDropdown.Item>
+                        <NavDropdown.Item href="#doesnotwork">Add Nutrition</NavDropdown.Item>
+                        <NavDropdown.Item href="#doesnotwork">another link</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Water" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#doesnotwork">Water Home</NavDropdown.Item>
+                        <NavDropdown.Item href="#doesnotwork">Add Water</NavDropdown.Item>
+                        <NavDropdown.Item href="#doesnotwork">another link</NavDropdown.Item>
+                    </NavDropdown>
                 </Nav>
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        Signed in as: <a href="#login">someone</a>
+                    </Navbar.Text>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
