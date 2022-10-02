4a6f6161cbdb7c9b166b60efc4261d5fe8e1d13a
Brandon Lee <blee2125@gmail.com>
2022-10-02 14:15:02 -0600

    start home page

---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const HomePage = () => {
+    
+    return(
+        <div>
+            home page - work in progress
+        </div>
+    )
+}
+
+export default HomePage;
