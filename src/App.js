036d33a978be308c6a15e0c6896f949ee707b323
Brandon Lee <blee2125@gmail.com>
2022-10-02 14:25:28 -0600

    make changes to router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  //Link,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Water from './components/Water/Water';
 import Food from './components/Food/Food';
@@ -18,14 +13,12 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/water" element={<Water />} />
           <Route path="/exercise" element={<Exercise />} />
           <Route path="/food" element={<Food />} />
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
