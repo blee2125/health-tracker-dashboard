1bd50e587f10911d6e0864fe83a35ce107cb5fa7
Brandon Lee <blee2125@gmail.com>
2022-10-11 18:37:57 -0600

    worked on update route

--- src/services/WaterService.js
+++ src/services/WaterService.js
@@ -9,13 +9,22 @@ class WaterService {
     return http.get(`/water/getOne/${id}`);
   }
 
+  getDate(dateString) {
+    return http.get(`/water/searchByDate/${dateString}`);
+  }
+
+  getDates(startDate, endDate) {
+    return http.get(`/water/getByDates/${startDate}-${endDate}`);
+  }
+
   create(data) {
     //console.log('create', data)
     return http.post("/water/post", data);
   }
 
   update(id, data) {
-    return http.put(`/update/${id}`, data);
+    console.log(id, data)
+    return http.put(`/water/update/${id}`, data);
   }
 
   delete(id) {
