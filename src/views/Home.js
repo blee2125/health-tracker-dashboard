750a4be15dc24cec3f6e412ffbae65a2bda12bf3
Brandon Lee <blee2125@gmail.com>
2022-10-12 11:37:04 -0600

    pass waterState to home view

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import { useSelector } from 'react-redux'
 
 const HomePage = () => {
+    const waterState = useSelector((state) => state.waterState)
     
     return(
         <div>
             home page - work in progress
+            <p>water - glasses {waterState.glasses}</p>
         </div>
     )
 }
