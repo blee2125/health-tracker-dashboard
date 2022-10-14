241e793c0975290c0e5dc5425fd423803e11bfb5
Brandon Lee <blee2125@gmail.com>
2022-10-14 13:36:01 -0600

    convert to functional component

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,60 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 import ExerciseForm from "./ExerciseForm";
 import ExerciseList from "./ExerciseList";
 
-export default class Exercise extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            exerciseObject: {
-                exerciseName: "",
-                duration: "",
-                timeOfExercise: "",
-                typeOfExercise: ""
-            },
-            exerciseArray: []
-        }
+import { connect } from "react-redux";
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleDeleteExercise = this.handleDeleteExercise.bind(this);
-    }
+import { createExercise } from "../../reducers/exerciseSlice";
 
-    updateData = (target, value) => {
-        this.setState(({
-            exerciseObject: {
-                ...this.state.exerciseObject,
-                [target]: value
-            }
-        }));
+function Exercise(props) {
+    const [exerciseObject, setExerciseObject] = useState({
+        exerciseName: "",
+        duration: '',
+        timeOfExercise: null,
+        typeOfExercise: null
+    })
+
+    const dateString = new Date().toString().split(' ')
+    const dateStringSplit = (`${dateString[1]} ${dateString[2]} ${dateString[3]}`).toString()
+
+    const updateData = (target, value) => {
+        let updatedValue = {};
+            updatedValue = {[target]: value};
+        setExerciseObject(exerciseObject => ({
+            ...exerciseObject,
+            ...updatedValue
+        }));        
     };
 
-    handleSubmit = () => {
-        if (this.state.exerciseObject.exerciseName !== '') {
-            this.setState(prevState => ({
-                exerciseArray: [...prevState.exerciseArray, this.state.exerciseObject]
-            }))
+    const handleSubmit = () => {
+        if (exerciseObject.exerciseName !== '') {
+
+            props.createExercise(exerciseObject)
+                .unwrap()
+                .then((data) => {
+                //console.log(data);
+                })
+                .catch((e) => {
+                console.log(e);
+                });
+
         }
     }
 
-    handleDeleteExercise(id) {
-        this.setState({
-            exerciseArray: this.state.exerciseArray.filter((_, i) => i !== id)
-        });
-    }
+    // handleDeleteExercise(id) {
+    //     this.setState({
+    //         exerciseArray: this.state.exerciseArray.filter((_, i) => i !== id)
+    //     });
+    // }
 
-    render() {
-      return (
+    // <div>
+    //         <h1>Exercise</h1>
+    //         <Card bg='light' border="secondary" style={{ width: '600px', padding: '25px', margin: "25px"}}>
+    //             <ExerciseForm  exerciseObject={this.state.exerciseObject} updateData={this.updateData} />
+    //             <Button onClick={this.handleSubmit}> Submit</Button>
+    //         </Card>
+    //         <ExerciseList list={this.state.exerciseArray} handleDelete={this.handleDeleteExercise} />
+    //     </div>
+
+    return (
         <div>
             <h1>Exercise</h1>
             <Card bg='light' border="secondary" style={{ width: '600px', padding: '25px', margin: "25px"}}>
-                <ExerciseForm  exerciseObject={this.state.exerciseObject} updateData={this.updateData} />
-                <Button onClick={this.handleSubmit}> Submit</Button>
+                <ExerciseForm  exerciseObject={exerciseObject} updateData={updateData} />
+                <Button onClick={handleSubmit}> Submit</Button>
             </Card>
-            <ExerciseList list={this.state.exerciseArray} handleDelete={this.handleDeleteExercise} />
         </div>
-      )
-    }
+    )
 }
+
+const mapStateToProps = (state) => {
+    return {
+        exerciseArray: state.exerciseState.exerciseArray
+    };
+  }
+
+export default connect(mapStateToProps, { createExercise })(Exercise)
