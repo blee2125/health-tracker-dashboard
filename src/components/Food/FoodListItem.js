633dcc8ff181e7ff5e9fb88f2ea1fc3b55b8ecc7
Brandon Lee <blee2125@gmail.com>
2022-10-14 16:53:44 -0600

    clean up code

--- src/components/Food/FoodListItem.js
+++ src/components/Food/FoodListItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 
+import Button from 'react-bootstrap/Button'
+
 function FoodListItem(props) {
 
-    function handleDeleteClick(id) {
-        console.log(id)
-      } //<button onClick={() => handleDeleteClick(props.id)}>X</button>
+    // function handleDeleteClick(id) {
+    //     console.log(id)
+    //   } //<button onClick={() => handleDeleteClick(props.id)}>X</button>
 
     return (
         <li>
             {props.foodData.foodName} {props.foodData.foodName} {props.foodData.foodName} {props.foodData.foodName} 
-            <button onClick={() => props.deleteButton(props.id)}> delete</button>
+            <Button onClick={() => props.deleteButton(props.id)}> delete</Button>
         </li>
     );
   }
