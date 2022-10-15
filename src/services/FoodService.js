cd182ccf30fe6eb1d39f16ceb5e54af845fa5302
Brandon Lee <blee2125@gmail.com>
2022-10-14 19:33:55 -0600

    start food routing

---
+++ src/services/FoodService.js
@@ -0,0 +1,44 @@
+import http from "../http-common";
+
+class FoodService {
+//   getAll() {
+//     return http.get("/food/getAll");
+//   }
+
+  // get(id) {
+  //   return http.get(`/food/getOne/${id}`);
+  // }
+
+//   getDate(data) {
+//     console.log(data)
+//     return http.get(`/food/searchByDate`, {params: data});
+//   }
+
+  // getDates(startDate, endDate) {
+  //   return http.get(`/food/getByDates/${startDate}-${endDate}`);
+  // }
+
+//   create(data) {
+//     console.log('create', data)
+//     return http.post("/food/post", data);
+//   }
+
+//   update(id, data) {
+//     console.log(id, data)
+//     return http.put(`/food/update/${id}`, data);
+//   }
+
+//   delete(id) {
+//     return http.delete(`/food/delete/${id}`);
+//   }
+
+  // deleteAll() {
+  //   return http.delete(`/food`);
+  // }
+
+  // findByTitle(title) {
+  //   return http.get(`/food?title=${title}`);
+  // }
+}
+
+export default new FoodService();
