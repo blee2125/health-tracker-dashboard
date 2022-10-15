24d2143995e8da345df9db302607fe9909b52c4b
Brandon Lee <blee2125@gmail.com>
2022-10-14 20:04:07 -0600

    add foodState to store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import waterReducer from './reducers/waterSlice'
 import exerciseReducer from './reducers/exerciseSlice'
+import foodReducer from './reducers/foodSlice'
 
 export default configureStore({
   reducer: {
     waterState: waterReducer,
-    exerciseState: exerciseReducer
+    exerciseState: exerciseReducer,
+    foodState: foodReducer
   },
 })
