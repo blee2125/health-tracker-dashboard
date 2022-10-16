3d152f734a55a5651be19081c89e2c1b63c3a964
Brandon Lee <blee2125@gmail.com>
2022-10-15 19:05:12 -0600

    add labels and dropdown

--- src/components/Exercise/ExerciseForm.js
+++ src/components/Exercise/ExerciseForm.js
@@ -4,36 +4,38 @@ export const ExerciseForm = (props) => {
 
     return (
       <form >
-        <label>add exercise</label>
-        <br />
+        <label>Name</label>
         <input 
           name='exerciseName' 
           placeholder='exerciseName'
           type='text'
           value={props.exerciseObject.exerciseName}
           onChange={e => props.updateData('exerciseName', e.target.value)}
-        />
+        /><br></br>
+        <label>Duration</label>
         <input 
           name='duration'
           placeholder='duration'
           type='number'
           value={props.exerciseObject.duration}
           onChange={e => props.updateData('duration', e.target.value)}
-        />
+        /><br></br>
+        <label>Time</label>
         <input 
           name='timeOfExercise'
           placeholder='timeOfExercise'
           type='time'
           value={props.exerciseObject.timeOfExercise}
           onChange={e => props.updateData('timeOfExercise', e.target.value)}
-        />
-        <input 
-          name='typeOfExercise'
-          placeholder='typeOfExercise'
-          type='text'
-          value={props.exerciseObject.typeOfExercise}
-          onChange={e => props.updateData('typeOfExercise', e.target.value)}
-        />
+        /><br></br>
+        <label>Type</label>
+        <select value={props.exerciseObject.typeOfExercise} onChange={e => props.updateData('typeOfExercise', e.target.value)}>
+          <option value=''></option>
+          <option value='Endurance'>Endurance</option>
+          <option value='Strength'>Strength</option>
+          <option value='Balance'>Balance</option>
+          <option value='Flexibility'>Flexibility</option>
+        </select>
       </form>
     );
   }
