01de36a4af54b612be13b7d4b902a06c0587fa48
Brandon Lee <blee2125@gmail.com>
2022-10-21 15:38:06 -0600

    add user to store

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import waterReducer from './reducers/waterSlice'
 import exerciseReducer from './reducers/exerciseSlice'
 import foodReducer from './reducers/foodSlice'
+import userReducer from './reducers/userSlice'
 
 export default configureStore({
   reducer: {
+    userState: userReducer,
     waterState: waterReducer,
     exerciseState: exerciseReducer,
     foodState: foodReducer
