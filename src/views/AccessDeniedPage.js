0d0078abd9f92c084c0d7b2f4b78c9b4d3435d30
Brandon Lee <blee2125@gmail.com>
2022-10-22 11:00:09 -0600

    create Access Denied page

---
+++ src/views/AccessDeniedPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AccessDeniedPage = () => {
+
+    return(
+        <div>
+            <h1>Access Denied</h1>
+
+            
+        </div>
+    )
+}
+
+export default (AccessDeniedPage);
