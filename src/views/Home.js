9ba34562faa528ed9471fb44edb81f1b2860abb4
Brandon Lee <blee2125@gmail.com>
2022-10-22 11:04:42 -0600

    imported UserContext and mapped data to view

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,18 +1,31 @@
 import React from 'react'
 
 import { useSelector } from 'react-redux'
+import UserContext from '../context/userContext';
+import { useContext } from 'react';
 
 const HomePage = (props) => {
+    const {userData} = useContext(UserContext);
     const waterState = useSelector((state) => state.waterState)
 
     const dateString = new Date().toString().split(' ')
     const dateStringSplit = (`${dateString[1]} ${dateString[2]} ${dateString[3]}`).toString()
+
+    const displayUserData = () => {
+        if (userData.user !== undefined) {
+            return userData.user.username
+        } else {
+            return 'not logged in'
+        }
+    }
 
     return(
         <div>
             <h1>{dateStringSplit}</h1>
 
             <p>water - glasses {waterState.glasses === 0 ?  "" : waterState.glasses}</p>
+            <p>username: {displayUserData()}</p>
+            <button onClick={()=>console.log(userData)}>userdata</button>
         </div>
     )
 }
