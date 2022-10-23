77e16e40145c345e2ed7a7ad4954a397fd4681ea
Brandon Lee <blee2125@gmail.com>
2022-10-23 16:38:00 -0600

    change delete button to delete modal

--- src/components/Food/FoodListItem.js
+++ src/components/Food/FoodListItem.js
@@ -1,6 +1,6 @@
 import React from "react";
-
 import Button from 'react-bootstrap/Button'
+import DeleteConfirmation from "../DeleteConfirmation";
 
 function FoodListItem(props) {
 
@@ -10,7 +10,10 @@ function FoodListItem(props) {
             <td>{props.foodData.calories}</td>
             <td>
                 <Button onClick={() => props.editButton(props.foodData._id)}>edit</Button>
-                <Button onClick={() => props.deleteButton(props.foodData._id)}>delete</Button>
+                <DeleteConfirmation 
+                    deleteItem={() => props.deleteButton(props.foodData._id)} 
+                    info={props.foodData.name} 
+                />
             </td>
         </tr>
     );
