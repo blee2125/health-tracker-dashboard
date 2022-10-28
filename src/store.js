8bfba9617846c446cbf83f576fd65994de893d2c
Brandon Lee <blee2125@gmail.com>
2022-10-28 13:11:17 -0600

    added weightState to store

--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import waterReducer from './reducers/waterSlice'
 import exerciseReducer from './reducers/exerciseSlice'
 import foodReducer from './reducers/foodSlice'
 import userReducer from './reducers/userSlice'
+import weightReducer from './reducers/weightSlice'
 
 export default configureStore({
   reducer: {
     userState: userReducer,
     waterState: waterReducer,
     exerciseState: exerciseReducer,
-    foodState: foodReducer
+    foodState: foodReducer,
+    weightState: weightReducer
   },
 })
