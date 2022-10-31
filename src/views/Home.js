f7f72d9aeb8c77d484d07c55f68caa8423d4a275
Brandon Lee <blee2125@gmail.com>
2022-10-31 11:59:16 -0600

    added water component and inline-flex style

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,9 +3,10 @@ import { useSelector } from 'react-redux'
 import WeightHome from '../components/BodyMetrics/Weight/WeightHome'
 import ExerciseHome from '../components/Exercise/ExerciseHome'
 import FoodHome from '../components/Food/FoodHome'
+import WaterHome from '../components/Water/WaterHome'
 
 const HomePage = (props) => {
-    const waterState = useSelector((state) => state.waterState)
+    const userData = useSelector((state) => state.userState.user)
 
     const dateString = new Date().toString().split(' ')
     const dateStringSplit = (`${dateString[1]} ${dateString[2]} ${dateString[3]}`).toString()
@@ -13,8 +14,11 @@ const HomePage = (props) => {
     return(
         <div>
             <h1>{dateStringSplit}</h1>
-            <p>Water - Glasses {waterState.glasses === 0 ?  "" : waterState.glasses}</p>
-            <WeightHome />
+            <p>Welcome {userData.username}</p>
+            <div style={{'display': 'inline-flex'}}>
+                <WaterHome />
+                <WeightHome />
+            </div>
             <ExerciseHome />
             <FoodHome />
         </div>
