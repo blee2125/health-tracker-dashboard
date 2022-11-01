e3b51632ad2840a0ff69a9c7c0e5cca249990388
Brandon Lee <blee2125@gmail.com>
2022-11-01 15:03:40 -0600

    created delete user and all data function

--- src/components/User/Settings/DeleteData.js
+++ src/components/User/Settings/DeleteData.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { connect, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Table, Card } from "react-bootstrap";
 import { deleteAllWater } from "../../../reducers/waterSlice";
 import { deleteAllWeight } from "../../../reducers/weightSlice";
 import { deleteAllFood } from "../../../reducers/foodSlice";
 import { deleteAllExercise } from "../../../reducers/exerciseSlice";
 import DeleteConfirmation from "../../../views/DeleteConfirmation";
+import { deleteUser } from "../../../reducers/userSlice";
+import { deleteSettings } from "../../../reducers/settingsSlice";
 
 
 const DeleteData = (props) => {
     const userToken = useSelector((state) => state.userState.user.token)
+    const navigate = useNavigate();
 
     const deleteWaterData = () => {
         props.deleteAllWater(userToken)
@@ -25,6 +29,16 @@ const DeleteData = (props) => {
 
     const deleteExerciseData = () => {
         props.deleteAllExercise(userToken)
+    }
+
+    const deleteAccountData = () => {
+        props.deleteAllExercise(userToken)
+        props.deleteAllFood(userToken)
+        props.deleteAllWater(userToken)
+        props.deleteAllWeight(userToken)
+        props.deleteSettings(userToken)
+        props.deleteUser(userToken)
+        navigate('/register')
     }
 
     return (
@@ -76,6 +90,17 @@ const DeleteData = (props) => {
                                     info={'All Weight Data'} 
                                 />
                             </td>
+                        </tr>  
+                        <tr>
+                            <td>
+                                Account User Data
+                            </td>
+                            <td>
+                                <DeleteConfirmation 
+                                    deleteItem={() => deleteAccountData(userToken)} 
+                                    info={'Account User Data'} 
+                                />
+                            </td>
                         </tr>                
                     </tbody>
                 </Table>
@@ -84,4 +109,4 @@ const DeleteData = (props) => {
     )
 }
 
-export default connect(null, {deleteAllWater, deleteAllExercise, deleteAllFood, deleteAllWeight}) (DeleteData)
+export default connect(null, {deleteAllWater, deleteAllExercise, deleteAllFood, deleteAllWeight, deleteUser, deleteSettings}) (DeleteData)
