6b2ed5003f7f9f5934cc6d173adccf1a647f18a6
Brandon Lee <blee2125@gmail.com>
2022-11-03 12:57:54 -0600

    move water from sidebar to seperate component

---
+++ src/components/Water/WaterSidebar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Button, ButtonGroup } from 'react-bootstrap';
+import { connect, useSelector } from "react-redux";
+import { createWater, updateWater, getWaterByDate } from "../../reducers/waterSlice";
+import DateFunctions from '../../functions/DateFunctions';
+
+const WaterSidebar = (props) => {
+    const glasses = useSelector((state) => state.waterState.glasses)
+    const id = useSelector((state) => state.waterState.id)
+    const userToken = useSelector((state) => state.userState.user ? state.userState.user.token : '')
+    const dateStringSplit = DateFunctions.createDateStringSplit()
+
+    const handleAddGlasses = () => {
+        if (id === null || undefined) {
+          handlePostRequest()
+        } else {
+        props.updateWater({id: id, data: {glasses: glasses+1}, token: userToken})
+          .unwrap()
+          .then((data) => {
+            
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+        }
+    };
+
+    const handleSubtractGlasses = () => {
+        props.updateWater({id: id, data: {glasses: glasses-1}, token: userToken})
+          .unwrap()
+          .then((data) => {
+            
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    };
+    
+    const handlePostRequest = () => {
+        props.createWater({data: {glasses: glasses + 1, date: dateStringSplit}, token: userToken})
+          .unwrap()
+          .then((data) => {
+            
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    }
+
+    return (
+        <ButtonGroup>
+            {glasses > 0 ? <Button variant="danger"  onClick={handleSubtractGlasses}>-</Button> : <Button variant="secondary" onClick={handleSubtractGlasses} disabled>-</Button>}
+            <Button variant="secondary" disabled>{glasses}</Button>
+            <Button onClick={handleAddGlasses}>+</Button>
+        </ButtonGroup>
+    )
+}
+
+export default connect(null, { createWater, updateWater, getWaterByDate }) (WaterSidebar)
