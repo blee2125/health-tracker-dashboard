7ada85c22f6ded9623092c5f5abebdddf286bc1c
Brandon Lee <blee2125@gmail.com>
2022-11-03 19:42:41 -0600

    added options for meals

--- src/components/Food/GraphComponents/FoodBarGraphSelection.js
+++ src/components/Food/GraphComponents/FoodBarGraphSelection.js
@@ -7,12 +7,17 @@ function FoodBarGraphSelection(props) {
     <>
         <Form>
             <Form.Group className="mb-3" controlId="formGroupGraphData">
-            <Form.Label>Graph Data</Form.Label>
+            <Form.Label>Graph Data - Meal</Form.Label>
                 <Form.Select
                     value={props.graphSelection}
                     onChange={e => props.setGraphSelection(e.target.value)}
                 >
-                    <option value='Calories'>Calories</option>
+                    <option value='All'>All</option>
+                    <option value='Breakfast'>Breakfast</option>
+                    <option value='Lunch'>Lunch</option>
+                    <option value='Dinner'>Dinner</option>
+                    <option value='Snack'>Snack</option>
+                    <option value=''>No Assigned Meal</option>
                 </Form.Select>
             </Form.Group>
         </Form>
