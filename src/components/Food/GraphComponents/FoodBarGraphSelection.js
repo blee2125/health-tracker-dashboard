dabac6c9bbf2d233308417970e9cb2d9181b9960
Brandon Lee <blee2125@gmail.com>
2022-11-04 09:17:01 -0600

    added macros selection

--- src/components/Food/GraphComponents/FoodBarGraphSelection.js
+++ src/components/Food/GraphComponents/FoodBarGraphSelection.js
@@ -1,16 +1,17 @@
 import React from "react";
-import {Form} from "react-bootstrap";
+import {Form, Card} from "react-bootstrap";
 
 function FoodBarGraphSelection(props) {
 
   return (
     <>
+        <Card bg='light' border="secondary" style={{ width: '300px', padding: '25px', margin: "25px"}}>
         <Form>
             <Form.Group className="mb-3" controlId="formGroupGraphData">
-            <Form.Label>Graph Data - Meal</Form.Label>
+            <Form.Label>Meal</Form.Label>
                 <Form.Select
                     value={props.graphSelection}
-                    onChange={e => props.setGraphSelection(e.target.value)}
+                    onChange={e => props.setGraphSelectionMeal(e.target.value)}
                 >
                     <option value='All'>All</option>
                     <option value='Breakfast'>Breakfast</option>
@@ -20,7 +21,20 @@ function FoodBarGraphSelection(props) {
                     <option value=''>No Assigned Meal</option>
                 </Form.Select>
             </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupGraphData">
+            <Form.Label>Calories/Macros</Form.Label>
+                <Form.Select
+                    value={props.GraphSelectionMacros}
+                    onChange={e => props.setGraphSelectionMacros(e.target.value)}
+                >
+                    <option value='calories'>Calories</option>
+                    <option value='carbsg'>Carbs</option>
+                    <option value='fatg'>Fat</option>
+                    <option value='proteing'>Protein</option>
+                </Form.Select>
+            </Form.Group>
         </Form>
+        </Card>
     </>
   )
 }
