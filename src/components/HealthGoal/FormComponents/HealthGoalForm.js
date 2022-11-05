a1b83cbb99cf33e0969bb867a03cf4f3d622b57b
Brandon Lee <blee2125@gmail.com>
2022-11-05 11:49:38 -0600

    switch data layout

--- src/components/HealthGoal/FormComponents/HealthGoalForm.js
+++ src/components/HealthGoal/FormComponents/HealthGoalForm.js
@@ -5,15 +5,6 @@ export const HealthGoalForm = (props) => {
 
     return (
       <Form >
-        <Form.Group className="mb-3" controlId="formGroupGoal">
-          <Form.Label>Goal</Form.Label>
-          <Form.Control 
-            type="text" 
-            placeholder="Goal" 
-            defaultValue={props.healthGoalObject.goal}
-            onChange={e => props.updateData('goal', e.target.value)}
-          />
-        </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupCategory">
           <Form.Label>Category</Form.Label>
           <Form.Select
@@ -28,6 +19,15 @@ export const HealthGoalForm = (props) => {
             <option value='Weight'>Weight</option>
             <option value='General'>General</option>
           </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupGoal">
+          <Form.Label>Goal</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Goal" 
+            defaultValue={props.healthGoalObject.goal}
+            onChange={e => props.updateData('goal', e.target.value)}
+          />
         </Form.Group>
       </Form>
     );
