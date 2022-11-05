adff92c7191f4125275b83437ef904473c41fe25
Brandon Lee <blee2125@gmail.com>
2022-11-05 11:48:05 -0600

    create filter for completed

--- src/components/HealthGoal/HealthGoal.js
+++ src/components/HealthGoal/HealthGoal.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect, useSelector } from "react-redux";
 import HealthGoalList from "./HealthGoalList";
 import { getAllHealthGoal, deleteHealthGoal } from "../../reducers/healthGoalSlice";
 import HealthGoalAddModal from './FormComponents/HealthGoalAddModal'
+import ListCompletedFilter from "./ListComponents/ListCompletedFilter";
 
 function HealthGoal(props) {
     const userToken = useSelector((state) => state.userState.user.token)
     const healthGoalArray = useSelector((state) => state.healthGoalState.healthGoalArray)
+    const [goalCompletedFilter, setGoalCompletedFilter] = useState('In Progress')
 
     const handleDeleteHealthGoal = (id) => {
         props.deleteHealthGoal({id: id, userToken: userToken})
@@ -15,6 +17,24 @@ function HealthGoal(props) {
             .catch((e) => {
                 console.log(e);
             });
+    }
+
+    function filterCompleted() {
+        const completeList = []
+        healthGoalArray.forEach(element => {
+            if (goalCompletedFilter === 'Completed') {
+                if (element.completed === true) {
+                    completeList.push(element)
+                }
+            } else if (goalCompletedFilter === 'In Progress') {
+                if (element.completed === false) {
+                    completeList.push(element)
+                }
+            } else {
+                completeList.push(element)
+            }
+        });
+        return completeList
     }
 
     useEffect(() => {
@@ -27,7 +47,8 @@ function HealthGoal(props) {
             
             <h1>Goals</h1>
             <HealthGoalList 
-                list={healthGoalArray}  
+                list={filterCompleted()}
+                listFilter={<ListCompletedFilter goalCompletedFilter={goalCompletedFilter} setGoalCompletedFilter={setGoalCompletedFilter} />}
                 handleDelete={handleDeleteHealthGoal}
                 addModal={<HealthGoalAddModal />}
             />
