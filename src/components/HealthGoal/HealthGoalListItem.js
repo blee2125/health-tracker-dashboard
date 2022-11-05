a585cca5e6c9232826f2329f7ef7c6e8a275b9ef
Brandon Lee <blee2125@gmail.com>
2022-11-04 19:32:34 -0600

    add button to mark completed

--- src/components/HealthGoal/HealthGoalListItem.js
+++ src/components/HealthGoal/HealthGoalListItem.js
@@ -1,13 +1,18 @@
 import React from "react";
 import DeleteConfirmation from "../Views/DeleteConfirmation";
 import HealthGoalEditModal from "./HealthGoalEditModal";
+import CompletedButton from "./CompletedButton";
 
 function HealthGoalListItem(props) {
 
     return (
         <tr>
             <td>{props.healthGoal.goal}</td>
+            <td>{props.healthGoal.category}</td>
+            <td>{props.healthGoal.completed ? 'yes' : 'no'}</td>
             <td>
+                {props.healthGoal.completed ? '' : <CompletedButton id={props.healthGoal._id}/>}
+            
                 <HealthGoalEditModal
                     healthGoalObject={props.healthGoal}
                 />
