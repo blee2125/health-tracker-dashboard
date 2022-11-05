431fc4f3536dae7e497f90129df4382ee00f6e26
Brandon Lee <blee2125@gmail.com>
2022-11-05 10:17:04 -0600

    replace edit/navigate button with edit/modal

--- src/components/Food/FoodListItem.js
+++ src/components/Food/FoodListItem.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Button from 'react-bootstrap/Button'
 import DeleteConfirmation from "../Views/DeleteConfirmation";
+import EditFoodModal from "./FormComponents/EditFoodModal";
 
 function FoodListItem(props) {
 
@@ -9,7 +9,9 @@ function FoodListItem(props) {
             <td><p>{props.foodData.name}</p></td>
             <td>{props.foodData.calories}</td>
             <td>
-                <Button onClick={() => props.editButton(props.foodData._id)}>Edit</Button>
+                <EditFoodModal
+                    foodObject={props.foodData}
+                />
                 <DeleteConfirmation 
                     deleteItem={() => props.deleteButton(props.foodData._id)} 
                     info={props.foodData.name} 
