0f63d59a39576249dabdf875dd7497a2db0ccb12
Brandon Lee <blee2125@gmail.com>
2022-11-04 18:58:43 -0600

    start HealthGoal component

---
+++ src/components/HealthGoal/HealthGoal.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { connect, useSelector } from "react-redux";
+import HealthGoalList from "./HealthGoalList";
+import { getAllHealthGoal, deleteHealthGoal } from "../../reducers/healthGoalSlice";
+import HealthGoalAddModal from './HealthGoalAddModal'
+
+function HealthGoal(props) {
+    const userToken = useSelector((state) => state.userState.user.token)
+    const healthGoalArray = useSelector((state) => state.healthGoalState.healthGoalArray)
+
+    const handleDeleteHealthGoal = (id) => {
+        props.deleteHealthGoal({id: id, userToken: userToken})
+            .unwrap()
+            .then((data) => {})
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+
+    useEffect(() => {
+        props.getAllHealthGoal(userToken)
+        // eslint-disable-next-line
+      }, [])
+
+    return (
+        <div>
+            
+            <h1>Goals</h1>
+            <HealthGoalList 
+                list={healthGoalArray}  
+                handleDelete={handleDeleteHealthGoal}
+                addModal={<HealthGoalAddModal />}
+            />
+        </div>
+    )
+}
+
+export default connect(null, { getAllHealthGoal, deleteHealthGoal })(HealthGoal)
