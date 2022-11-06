0ebaf4ceca4577fe6afbf15fc4da4fc1a2509a03
Brandon Lee <blee2125@gmail.com>
2022-11-06 08:09:57 -0700

    update chart to display no data message

--- src/components/Food/GraphComponents/FoodMacrosPieChart.js
+++ src/components/Food/GraphComponents/FoodMacrosPieChart.js
@@ -45,25 +45,42 @@ function FoodMacrosPieChart(props) {
         }]
       };
 
+      const plugins2 = [
+        {
+          afterDraw: function (chart) {
+            if (chart.config._config.data.datasets[0].data.filter(d => d>0).length < 1) {
+              let ctx = chart.ctx;
+              let width = chart.width;
+              let height = chart.height;
+              ctx.textAlign = "center";
+              ctx.textBaseline = "middle";
+              ctx.font = "30px Arial";
+              ctx.fillText("No data to display", width / 2, height / 2);
+              ctx.restore();
+            }
+          },
+        },
+      ];
+
     return (
         <>
         <Card bg='light' border="secondary" style={{ width: '400px', padding: '25px', margin: "25px"}}>
             <Chart
                 type="pie"
                 data={graphData}
+                plugins={plugins2}
                 options={{
-                    
-                plugins: {
-                    title:{
-                    display:true,
-                    text:`Macros (Total Calories)`,
-                    fontSize:20
-                    },
-                    legend:{
-                    display:true,
-                    position:'bottom'
+                    plugins: {
+                        title:{
+                        display:true,
+                        text:`Macros (Total Calories)`,
+                        fontSize:20
+                        },
+                        legend:{
+                        display:true,
+                        position:'bottom'
+                        }
                     }
-                }
                 }}
             />
             </Card>
