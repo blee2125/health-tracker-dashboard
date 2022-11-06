45f3e15912250f74e976ffa01f0453528fd369c0
Brandon Lee <blee2125@gmail.com>
2022-11-06 12:02:23 -0700

    remove unused code

--- src/reducers/notificationSlice.js
+++ src/reducers/notificationSlice.js
@@ -20,40 +20,12 @@ export const notificationSlice = createSlice({
         state.message = action.payload.message
         state.type = 'success'
     },
-    primary: (state, action) => {
-      state.message = action.payload.message
-      state.type = 'primary'
-    },
-    danger: (state, notification) => {
-        state.message = notification
-        state.type = 'danger'
-    },
-    secondary: (state, notification) => {
-        state.message = notification
-        state.type = 'secondary'
-    },
-    warning: (state, notification) => {
-        state.message = notification
-        state.type = 'warning'
-    },
-    info: (state, notification) => {
-        state.message = notification
-        state.type = 'info'
-    },
-    light: (state, notification) => {
-        state.message = notification
-        state.type = 'light'
-    },
-    dark: (state, notification) => {
-        state.message = notification
-        state.type = 'dark'
-    },
   },
   extraReducers: {
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { notify, danger, success, primary, secondary, warning, info, light, dark, notificationReset } = notificationSlice.actions
+export const { notify, success, notificationReset } = notificationSlice.actions
 
 export default notificationSlice.reducer
