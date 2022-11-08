c3d657f3820d9d624e37871c6bb26cdac5b9ac85
Brandon Lee <blee2125@gmail.com>
2022-11-07 20:12:37 -0700

    create component to compare water goal and water drank for notifications

---
+++ src/components/Views/Notifications/WaterGoalNotification.js
@@ -0,0 +1,31 @@
+import React, {useEffect} from "react";
+import { useSelector, useDispatch, connect } from "react-redux";
+import { notify } from "../../../reducers/notificationSlice";
+
+function WaterGoalNotification() {
+    const waterGoal = useSelector((state) => state.healthGoalState.waterGoal)
+    const glasses = useSelector((state) => state.waterState.glasses)
+    const dispatch = useDispatch()
+    const water = glasses.toString()
+    
+    function goalAcheived() {
+        if (water < waterGoal.goal) {
+            dispatch(notify({message: `Goal Progress: ${glasses} of ${waterGoal.goal} glasses`, type: 'secondary'}))
+        } else if (waterGoal.goal === glasses.toString()) {
+            dispatch(notify({message: `Goal Complete: ${waterGoal.goal} glasses`, type: 'success'}))
+        }
+    }
+
+    useEffect(() => {
+        goalAcheived()
+        // eslint-disable-next-line
+    }, [water])
+
+    return(
+        <div>
+            water goals - {waterGoal.goal}
+        </div>
+    )
+}
+
+export default connect(null, {notify}) (WaterGoalNotification)
