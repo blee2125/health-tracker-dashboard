cc9f2d4cccc6f5fed465c7b4c6952b52e23edc64
Brandon Lee <blee2125@gmail.com>
2022-11-07 17:48:57 -0700

    move

---
+++ src/components/HealthGoal/ListComponents/CompletedButton.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { connect, useSelector } from "react-redux";
+import { updateHealthGoal, getAllHealthGoal } from "../../../reducers/healthGoalSlice";
+
+function CompletedButton(props) {
+    const userToken = useSelector((state) => state.userState.user.token)
+
+    const markCompleted = () => {
+        props.updateHealthGoal({id: props.id, data: {completed: true}, userToken: userToken})
+            .unwrap()
+            .then((data) => {
+                props.getAllHealthGoal(userToken)
+            })
+            .catch((e) => {console.log(e)});
+    }
+
+
+    return (
+        <Button onClick={markCompleted} variant="success">Done</Button>
+    )
+}
+
+export default connect(null, { updateHealthGoal, getAllHealthGoal }) (CompletedButton)
