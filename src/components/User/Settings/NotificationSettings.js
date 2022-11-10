e063f0dcb583e3303eeef9a25eca0c50c4a1c182
Brandon Lee <blee2125@gmail.com>
2022-11-10 13:22:31 -0700

    update notification settings and add functionality

--- src/components/User/Settings/NotificationSettings.js
+++ src/components/User/Settings/NotificationSettings.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Card } from "react-bootstrap";
+import { Form, Card, Button, ButtonGroup } from "react-bootstrap";
 import { connect, useSelector, useDispatch } from "react-redux";
 import { toggleSettings } from "../../../reducers/settingsSlice";
 import {notify} from '../../../reducers/notificationSlice'
@@ -8,29 +8,13 @@ function NotificationSettings(props) {
     const dispatch = useDispatch();
     const settings = useSelector((state) => state.settingsState.settings);
 
-    const exerciseSettingSwitch = () => {
-        dispatch(toggleSettings({setting: 'exerciseNotification'}))
-        dispatch(notify({message: 'Exercise Notification Updated',type: 'success'}))
+    const onNotifications = () => {
+        dispatch(toggleSettings({setting: 'onNotifications'}))
+        dispatch(notify({message: 'All Notifications On',type: 'success'}))
     }
 
-    const foodSettingSwitch = () => {
-        dispatch(toggleSettings({setting: 'foodNotification'}))
-        dispatch(notify({message: 'Food Notification Updated',type: 'success'}))
-    }
-
-    const waterSettingSwitch = () => {
-        dispatch(toggleSettings({setting: 'waterNotification'}))
-        dispatch(notify({message: 'Water Notification Updated',type: 'success'}))
-    }
-
-    const weightSettingSwitch = () => {
-        dispatch(toggleSettings({setting: 'weightNotification'}))
-        dispatch(notify({message: 'Weight Notification Updated',type: 'success'}))
-    }
-
-    const goalSettingSwitch = () => {
-        dispatch(toggleSettings({setting: 'goalNotification'}))
-        dispatch(notify({message: 'Goal Notification Updated',type: 'success'}))
+    const offNotifications = () => {
+        dispatch(toggleSettings({setting: 'offNotifications'}))
     }
 
     return (
@@ -38,41 +22,16 @@ function NotificationSettings(props) {
             <Card bg='light' border="secondary" style={{ width: '300px', padding: '25px', margin: "25px"}}>
                 <h3>Notifications</h3>
                 <Form >
-                    <Form.Check 
-                        type="switch"
-                        id="custom-switch"
-                        checked={settings.exerciseNotification}
-                        onChange={() => exerciseSettingSwitch()}
-                        label="Exercise"
-                    />
-                    <Form.Check 
-                        type="switch"
-                        id="custom-switch"
-                        checked={settings.foodNotification}
-                        onChange={() => foodSettingSwitch()}
-                        label="Food"
-                    />
-                    <Form.Check 
-                        type="switch"
-                        id="custom-switch"
-                        checked={settings.waterNotification}
-                        onChange={() => waterSettingSwitch()}
-                        label="Water"
-                    />
-                    <Form.Check 
-                        type="switch"
-                        id="custom-switch"
-                        checked={settings.weightNotification}
-                        onChange={() => weightSettingSwitch()}
-                        label="Weight"
-                    />
-                    <Form.Check 
-                        type="switch"
-                        id="custom-switch"
-                        checked={settings.goalNotification}
-                        onChange={() => goalSettingSwitch()}
-                        label="Goal"
-                    />
+                    <ButtonGroup>
+                        <Button 
+                            variant={settings.notifications ? 'primary' : 'secondary'}
+                            onClick={onNotifications}>
+                        On</Button>
+                        <Button
+                            variant={settings.notifications ? 'secondary' : 'primary'}
+                            onClick={offNotifications}>
+                        Off</Button>
+                    </ButtonGroup>
                 </Form>
             </Card>
         </>
