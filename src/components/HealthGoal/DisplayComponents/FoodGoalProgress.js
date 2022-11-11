dbf994c9caf1540703da86fd2b2a1fb247468c62
Brandon Lee <blee2125@gmail.com>
2022-11-10 17:06:08 -0700

    create food progress card

---
+++ src/components/HealthGoal/DisplayComponents/FoodGoalProgress.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import GoalProgressCard from "./GoalProgressCard";
+
+function FoodGoalProgress() {
+    const foodGoal = useSelector((state) => state.healthGoalState.foodGoal.goal)
+    const foodTodayArray = useSelector((state) => state.foodState.foodTodayArray)
+
+    const calcTotal = foodTodayArray.reduce((accumulator, food) => {
+        const cal = food.calories > 0 ? Math.round(food.calories) : 0
+        return (accumulator + cal);
+    }, 0);
+
+    return (
+        <div>
+            <GoalProgressCard
+                title={'Daily Calories'}
+                goalUnit={'Calories'}
+                goal={calcTotal}
+                total={foodGoal}
+            />
+        </div>
+    )
+}
+
+export default (FoodGoalProgress)
