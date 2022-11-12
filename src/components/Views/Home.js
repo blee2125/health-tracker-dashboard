271d1c75d41e0a142e8c1ee28ba4ef83088bac35
Brandon Lee <blee2125@gmail.com>
2022-11-12 16:32:46 -0700

    added FoodMealData

--- src/components/Views/Home.js
+++ src/components/Views/Home.js
@@ -12,9 +12,11 @@ import WaterHome from '../Water/WaterHome'
 import FoodGoalProgress from '../HealthGoal/DisplayComponents/FoodGoalProgress'
 import WaterGoalProgress from '../HealthGoal/DisplayComponents/WaterGoalProgress'
 import WeightGoalProgress from '../HealthGoal/DisplayComponents/WeightGoalProgress'
+import FoodMealData from '../Food/DisplayComponents/FoodMealData'
 
 const HomePage = (props) => {
     const userData = useSelector((state) => state.userState.user)
+    const foodTodayArray = useSelector((state) => state.foodState.foodTodayArray)
 
     const dateStringSplit = DateFunctions.createDateStringSplit()
 
@@ -34,7 +36,7 @@ const HomePage = (props) => {
                 <Col lg='auto'><WaterHome /></Col>
             </Row>
             <Row>
-                
+                <FoodMealData foodData={foodTodayArray} />
                 <FoodHome />
                 <ExerciseGraph />
                 <ExerciseHome />
