664dbbf71fae93802226e3a940ab1f11a4e78ab8
Brandon Lee <blee2125@gmail.com>
2022-11-13 17:02:32 -0700

    change background to match legend

--- src/components/BodyMetrics/BloodPressure/ListComponents/BloodPressureListItem.js
+++ src/components/BodyMetrics/BloodPressure/ListComponents/BloodPressureListItem.js
@@ -3,8 +3,24 @@ import DeleteConfirmation from "../../../Views/DeleteConfirmation";
 
 function BloodPressureListItem(props) {
 
+    const backgroundColor = () => {
+        const upper = props.bloodPressure.systolic
+        const lower = props.bloodPressure.diastolic
+        if (upper >= 180 || lower >= 120) {
+            return 'red'
+        } else if ((upper >= 140 & upper < 180) || (lower >= 90 & lower < 120)) {
+            return 'orangered'
+        } else if ((upper >= 130 & upper < 140) || (lower >= 80 & lower < 90)) {
+            return 'orange'
+        } else if (upper >= 120 & upper < 130 & lower < 80) {
+            return 'yellow'
+        } else if (upper < 120 & lower < 80) {
+            return 'lightgreen'
+        }
+    }
+
     return (
-        <tr>
+        <tr style={{background: backgroundColor()}}>
             <td>{props.bloodPressure.systolic}</td>
             <td>{props.bloodPressure.diastolic}</td>
             <td>{props.bloodPressure.heartRate}</td>
