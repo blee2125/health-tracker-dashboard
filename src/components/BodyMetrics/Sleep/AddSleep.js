ce90f8115ad10a9563f5c867358d4c28103daa3f
Brandon Lee <blee2125@gmail.com>
2022-11-13 17:50:51 -0700

    added totalTime - automatically calculated

--- src/components/BodyMetrics/Sleep/AddSleep.js
+++ src/components/BodyMetrics/Sleep/AddSleep.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Modal, NavLink } from 'react-bootstrap';
 import { createSleep } from "../../../reducers/sleepSlice";
 import { connect, useSelector, useDispatch } from "react-redux";
@@ -10,7 +10,8 @@ function AddSleep(props) {
     const userToken = useSelector((state) => state.userState.user.token)
     const [sleepObject, setSleepObject] = useState({
         bedtime: '',
-        wakeUpTime: ''
+        wakeUpTime: '',
+        totalTime: ''
     })
     
     const [show, setShow] = useState(false);
@@ -29,14 +30,34 @@ function AddSleep(props) {
         .catch((e) => {console.log(e)});
     }
 
+    function calcTotalTime() {
+        let total = 0
+        const bedtimeHrMin = sleepObject.bedtime.split(':')
+        const bedtimeToMin = (Number(bedtimeHrMin[0])*60) + Number(bedtimeHrMin[1])
+        const wakeHrMin = sleepObject.wakeUpTime.split(':')
+        const wakeToMin = (Number(wakeHrMin[0])*60) + Number(wakeHrMin[1])
+        const difference = ((Number(wakeToMin) - Number(bedtimeToMin))/60)
+        if (difference < 0) {
+            total = Math.round((24+difference)*60)
+        } else {
+            total = Math.round(difference * 60)
+        }
+        sleepObject.totalTime = total
+    }
+
     const updateData = (target, value) => {
         let updatedValue = {};
             updatedValue = {[target]: value};
         setSleepObject(sleepObject => ({
             ...sleepObject,
             ...updatedValue
-        }));        
+        }));
     };
+
+    useEffect(() => {
+        calcTotalTime()
+        // eslint-disable-next-line
+    }, [sleepObject])
 
     return (
         <>
